7755f9a9ffad36d8fc6499ee3a9389f0b61b9de1
NicolasPanda <91343504+NicolasPanda@users.noreply.github.com>
2023-02-25 16:02:34 -0500

    base

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="flex flex-col">
+      <h1 className="text-xl text-red-500">Hello</h1>
+    </div>
+  );
+}
+
+export default App;
